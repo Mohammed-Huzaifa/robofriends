12e15c5cb76f13ffc323d33858e7a1624df3545d
Huzaifa <mohammedhuzaifa857@gmail.com>
2023-07-06 07:53:09 -0700

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Mohammed Huzaifa',
+      username: 'amfamo',
+      email: 'amfamo@.in'
+    },
+    {
+      id: 2,
+      name: 'Montu',
+      username: 'musafir',
+      email: 'musafir25.co'
+    },
+    {
+      id: 3,
+      name: 'Hash',
+      username: 'weirdo',
+      email: 'weirdo.net'
+    },
+    {
+      id: 4,
+      name: 'Hamza',
+      username: 'hamzu',
+      email: 'hamzu.org'
+    },
+    {
+      id: 5,
+      name: 'Ameen',
+      username: 'ameen.tannu',
+      email: 'ameenhuzaifa.ca'
+    },
+    {
+      id: 6,
+      name: 'Fathima',
+      username: 'tannu.ameen',
+      email: 'hashham.info'
+    },
+    {
+      id: 7,
+      name: 'Sadi',
+      username: 'saadi.ithiaaz',
+      email: 'saadi@billy.biz'
+    },
+    {
+      id: 8,
+      name: 'Talha',
+      username: 'talhu',
+      email: 'talha@rosamond.me'
+    },
+    {
+      id: 9,
+      name: 'Taha',
+      username: 'Tahu',
+      email: 'taha@dana.io'
+    },
+    {
+      id: 10,
+      name: 'Wasiq',
+      username: 'wasiq.mohideen',
+      email: 'wasiq@karina.biz'
+    }
+  ];
+  
